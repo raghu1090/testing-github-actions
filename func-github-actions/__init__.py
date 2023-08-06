27d8b8cd60e999f90ea13ad33343c24ccfa3fa5f
raghu <raghu.monavarthi9@gmail.com>
2023-08-06 11:34:34 +0530

    changes to triggered line

--- func-github-actions/__init__.py
+++ func-github-actions/__init__.py
@@ -19,6 +19,6 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered dayyyyyyyyyyy. Pass a name in the query string or in the request body for a personalized response.",
+             "This is very great awesome day. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
